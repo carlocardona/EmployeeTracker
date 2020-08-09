859b7a7392bb286b44a315f9a5cd223ed2922ada
Carlo <pbjunkiep3@yahoo.com>
2020-08-09 21:10:14 +1000

    have basic functionality skeleton, next SQL commands

--- emptrack.js
+++ emptrack.js
@@ -3,11 +3,8 @@ const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
     host: "localhost",
-
     port: 3306,
-
     user: "root",
-
     password: "forgo-reason",
     database: "employee_tracker"
 });
@@ -19,8 +16,39 @@ connection.connect(err => {
 });
 
 const main = () => {
-    console.log('Main Function');
-    end();
+    inquirer.prompt([
+        {
+            type:'list',
+            name: 'action',
+            message: 'What would you like to do',
+            choices:['View All Employees','Add an Employee','Quit']
+        }
+    ]).then(response =>{
+        console.log(response);
+
+        const choice = response.action
+
+        switch(choice){
+            case 'View All Employees': allEmployees();
+            break;
+            case 'Add an Employee': addEmployee();
+            break;
+            case 'Quit': end();
+            break;
+            default: end();
+        }
+        
+    });
+}
+
+const allEmployees = () =>{
+    console.log('All Employees');
+    main();
+}
+
+const addEmployee = () =>{
+    console.log('Add Employee');
+    main();
 }
 
 const end = () => {
