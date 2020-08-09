25353d48d1e854de4335c267fd52e51520222da5
Carlo <pbjunkiep3@yahoo.com>
2020-08-09 18:54:45 +1000

    created and ended a server connection

--- emptrack.js
+++ emptrack.js
@@ -4,13 +4,26 @@ const inquirer = require("inquirer");
 const connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
     port: 3306,
 
-    // Your username
     user: "root",
 
-    // Your password
     password: "forgo-reason",
     database: "employee_tracker"
 });
+
+connection.connect(err => {
+    if(err) throw err;
+    console.log('connect as id: ' + connection.threadId);
+    main();
+});
+
+const main = () => {
+    console.log('Main Function');
+    end();
+}
+
+const end = () => {
+    console.log('End Connection');
+    connection.end();
+}
